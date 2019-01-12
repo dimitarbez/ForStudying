d5f33aa3bf728ead4ac942eeb02733e215d7ebc5
Dimitar Bezanovski <30288047+dimitarbez@users.noreply.github.com>
2019-01-12 22:58:41 +0100

    palindrome

---
+++ palindrome/palindrome.cpp
@@ -0,0 +1,42 @@
+// palindrome.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindrome;
+
+void palindromeTester(string s)
+{
+	int n = s.length();
+	for (int i = 0; i < n/2; i++)
+	{
+		if (s[i] != s[(n - 1) - i])
+		{
+			isPalindrome = false;
+		}
+		else
+		{
+			isPalindrome = true;
+		}
+	}
+	if (isPalindrome)
+	{
+		cout << "ya boy e palndrom\n";
+	}
+	else
+	{
+		cout << "ya boy ne e palndrom\n";
+	}
+}
+
+int main()
+{
+	string s;
+	cin >> s;
+	palindromeTester(s);
+}
+
